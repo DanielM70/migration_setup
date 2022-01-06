85f6f19b3d3e24c144b47951fdb4a92a47668eb5
Daniel Mulrooney <daniel.mulrooney@pivotree.com>
2022-01-05 20:45:03 -0500

    Refactor file names

--- migrationFactorySetup.py
+++ migrationFactorySetup.py
@@ -5,13 +5,18 @@ import git
 
 
 Account_ID = boto3.client('sts').get_caller_identity()['Account']
+
 bucketName = Account_ID + 'migration'
-solutionName = 'migrationFactory'
-version = '0.01'
-args = './build-s3-dist.sh' + ' ' + bucketName + ' ' + solutionName + ' ' + version 
+solutionName = 'migrationFactory'       ## Replace with customer 3 letter acronym
+version = '0.01'                        ## should be incremented by platform only
+
+
+cmd = './build-s3-dist.sh' + ' ' + bucketName + ' ' + solutionName + ' ' + version 
 
 #git.Repo.clone_from('git@github.com:awslabs/aws-cloudendure-migration-factory-solution.git', 'MigrationFactory')
 #subprocess.call(['chmod', '0755', '/Users/daniel.mulrooney/workspaces/migration_setup/MigrationFactory/deployment/build-s3-dist.sh'])
-#os.system('/Users/daniel.mulrooney/workspaces/migration_setup/MigrationFactory/deployment/build-s3-dist.sh {} {} {}' .format(str(bucketName), str(solutionName), str(version)) )
+#ubprocess.check_call([cmd], shell=True, cwd='/Users/daniel.mulrooney/workspaces/migration_setup/MigrationFactory/deployment/')
+subprocess.call(['chmod', '0755', '/Users/daniel.mulrooney/workspaces/migration_setup/fixScript.sh'])
+subprocess.check_call('./fixScript.sh', shell=True, cwd='/Users/daniel.mulrooney/workspaces/migration_setup/')
 
-subprocess.check_call([args], shell=True, cwd='/Users/daniel.mulrooney/workspaces/migration_setup/MigrationFactory/deployment/')
+
